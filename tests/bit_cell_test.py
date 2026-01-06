910f99902fc099f53b6f72e1b9dab64dcaecf182
ImRuliad <nicholaseovva@csus.edu>
2026-01-05 22:13:00 -0800

    test: write high

--- tests/bit_cell_test.py
+++ tests/bit_cell_test.py
@@ -6,6 +6,15 @@ HIGH = 1
 CLOCK_PERIODS = 10
 NANOSECONDS = "ns"
 
+async def reset_sequence(dut):
+    dut.rst.value = HIGH
+    dut.write_enable.value = LOW
+    dut.data_in.value = LOW
+    dut.clk.value = LOW
+    await Timer(2, units=NANOSECONDS)
+    dut.rst.value = LOW
+    await Timer(1, units=NANOSECONDS)
+
 @cocotb.test()
 async def smoke_test(dut):
     for _ in range (CLOCK_PERIODS):
@@ -22,3 +31,15 @@ async def test_reset_state(dut):
     await Timer(2, unit=NANOSECONDS)
     assert dut.data_out.value == LOW, f"Reset failed: data_out should be {LOW}, got {dut.data_out.value}"
     cocotb.log.info("Reset state passed!")
+
+@cocotb.test()
+async def test_write_high(dut):
+    await reset_sequence(dut)
+    dut.write_enable.value = HIGH
+    dut.data_in.value = HIGH
+    dut.clk.value = LOW
+    await Timer(1, units=NANOSECONDS)
+    dut.clk.value = HIGH
+    await Timer(1, units=NANOSECONDS)
+    assert dut.data_out.value == HIGH, f"Write High failed: data_out should be {HIGH}, got {dut.data_out.value}"
+
