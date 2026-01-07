7c9079c970cdb74129ff89f0109dfb3f326daf46
ImRuliad <nicholaseovva@csus.edu>
2026-01-07 13:37:09 -0800

    fix: row decoder logic

--- cell_array.sv
+++ cell_array.sv
@@ -12,6 +12,14 @@ module cell_array #(
 
     wire [ROWS-1:0][COLS-1:0] total_data_out; //final output of every cell in all ROWS and COLS
 
+    wire [ROWS-1:0] row_decoder;
+    genvar r;
+    generate
+        for(r = 0; r < ROWS; r++) begin : gen_decoder
+            assign row_decoder[r] = (row_select == r);
+        end
+    endgenerate
+
     genvar curr_row, curr_col;
     generate
         for (curr_row = 0; curr_row < ROWS; curr_row++) begin : gen_rows
@@ -19,7 +27,7 @@ module cell_array #(
                 bit_cell cell (
                     .clk(clk),
                     .rst(rst),
-                    .write_enable((row_select == curr_row) & col_write_enable[curr_col]),
+                    .write_enable(row_decoder[curr_row] & col_write_enable[curr_col]),
                     .data_in(col_data_in[curr_col]),
                     .data_out(total_data_out[curr_row][curr_col])
                 );
@@ -27,5 +35,9 @@ module cell_array #(
         end
     endgenerate
 
-
-
+    genvar c;
+    generate
+        for (c = 0; c < COLS; c++) begin : mux
+            assign col_data_out[c] = total_data_out[row_select][c];
+        end
+    endgenerate
