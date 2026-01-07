6e75bd7959c626b891ced116170ee7ec96d5687f
ImRuliad <nicholaseovva@csus.edu>
2026-01-07 08:26:14 -0800

    refactor: set and reset states

--- tests/bit_cell_test.py
+++ tests/bit_cell_test.py
@@ -3,7 +3,6 @@ from cocotb.triggers import Timer
 
 LOW = 0
 HIGH = 1
-CLOCK_PERIODS = 10
 NANOSECONDS = "ns"
 
 async def reset_sequence(dut):
@@ -11,45 +10,53 @@ async def reset_sequence(dut):
     dut.write_enable.value = LOW
     dut.data_in.value = LOW
     dut.clk.value = LOW
-    await Timer(2, units=NANOSECONDS)
+    await Timer(2, unit=NANOSECONDS)
     dut.rst.value = LOW
-    await Timer(1, units=NANOSECONDS)
+    await Timer(1, unit=NANOSECONDS)
 
+async def generate_clock_cycle(dut):
+    dut.clk.value = LOW
+    await Timer(1, unit=NANOSECONDS)
+    dut.clk.value = HIGH
+    await Timer(1, unit=NANOSECONDS)
+
+async def write_data(dut, data):
+    dut.write_enable.value = HIGH
+    dut.data_in.value = data
+
+
+# ============================================= BASIC FUNCTIONALITY TESTS =============================================
+
+#ensure ff can be clocked.
 @cocotb.test()
 async def smoke_test(dut):
+    CLOCK_PERIODS = 1000
     for _ in range (CLOCK_PERIODS):
-        dut.clk.value = LOW
-        await Timer(1, unit=NANOSECONDS)
-        dut.clk.value = HIGH
-        await Timer(1, unit=NANOSECONDS)
+      await generate_clock_cycle(dut)
 
+#ensure ff state can be reset.
 @cocotb.test()
 async def test_reset_state(dut):
     dut.rst.value = HIGH
     dut.write_enable.value = LOW
     dut.data_in.value = LOW
     await Timer(2, unit=NANOSECONDS)
-    assert dut.data_out.value == LOW, f"Reset failed: data_out should be {LOW}, got {dut.data_out.value}"
+    assert dut.data_out.value == LOW, f"Reset failed: EXPECTED {LOW}, GOT {dut.data_out.value}"
     cocotb.log.info("Reset state passed!")
 
+#ensure ff can output active high.
 @cocotb.test()
 async def test_write_high(dut):
     await reset_sequence(dut)
-    dut.write_enable.value = HIGH
-    dut.data_in.value = HIGH
-    dut.clk.value = LOW
-    await Timer(1, units=NANOSECONDS)
-    dut.clk.value = HIGH
-    await Timer(1, units=NANOSECONDS)
-    assert dut.data_out.value == HIGH, f"Write High failed: data_out should be {HIGH}, got {dut.data_out.value}"
+    await write_data(dut, HIGH)
+    await generate_clock_cycle(dut)
+    assert dut.data_out.value == HIGH, f"Write High failed: EXPECTED {HIGH}, GOT {dut.data_out.value}"
 
+#ensure ff can output active low.
 @cocotb.test()
 async def test_write_low(dut):
     await reset_sequence(dut)
-    dut.write_enable.value = HIGH
-    dut.data_in.value = LOW
-    dut.clk.value = LOW
-    await Timer(1, units=NANOSECONDS)
-    dut.clk.value = HIGH
-    await Timer(1, units=NANOSECONDS)
-    assert dut.data_out.value == LOW, f"Write Low failed: data_out should be {LOW}, got {dut.data_out.value}"
+    await write_data(dut, LOW)
+    await generate_clock_cycle(dut)
+    assert dut.data_out.value == LOW, f"Write Low failed: EXPECTED {LOW}, GOT {dut.data_out.value}"
+
