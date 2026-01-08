c8baf8fd0ec1f07175a5e83abfb6fea6f6de5378
ImRuliad <nicholaseovva@csus.edu>
2026-01-08 11:47:24 -0800

    adjust constants for column decoding logic

--- tests/constants.py
+++ tests/constants.py
@@ -6,18 +6,20 @@ This file should contain all constants used in testbenching.
 #Hardware parameters (MUST MATCH WITH RTL)
 ROWS = 64
 COLS = 64
-ADDR_WIDTH = math.log2(ROWS)
+DATA_WIDTH = 8
+ROW_ADDR_WIDTH = math.ceil(math.log2(ROWS))
+COL_ADDR_WIDTH = math.ceil(math.log2(COLS // DATA_WIDTH))
 
 #Test timing constants
 NANOSECONDS = "ns"
-CLOCK_PERIOD_NS = 1000000
+CLOCK_PERIOD_NS = 1000
 
 #Logic Levels
 LOW = 0
 HIGH = 1
 
-#Common Data Patterns
-ALL_ZEROES = 0x0000000000000000
-ALL_ONES = 0xFFFFFFFFFFFFFFFF
-CHECKERBOARD_A = 0x5555555555555555  
-CHECKERBOARD_B = 0xAAAAAAAAAAAAAAAA
+# 8-bit data patterns
+ALL_ZEROES = 0x00
+ALL_ONES = 0xFF
+CHECKERBOARD_A = 0x55  # 01010101
+CHECKERBOARD_B = 0xAA  # 10101010
