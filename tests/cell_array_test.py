72989abf4e3cc9f0db28b22cf3051f7caf1d8682
ImRuliad <nicholaseovva@csus.edu>
2026-01-08 17:20:10 -0800

    fix: type hints

--- tests/cell_array_test.py
+++ tests/cell_array_test.py
@@ -1,39 +1,43 @@
 import cocotb
 import tests.constants as constants
 from cocotb.triggers import Timer
+from cocotb.handle import SimHandleBase
 
 # ==================== HELPER FUNCTIONS ====================
 
-async def generate_clock_cycle(dut):
+async def generate_clock_cycle(dut: SimHandleBase) -> None:
     dut.clk.value = constants.LOW
     await Timer(1, unit=constants.NANOSECONDS)
     dut.clk.value = constants.HIGH
     await Timer(1, unit=constants.NANOSECONDS)
 
-async def reset_cell_array(dut):
+async def reset_cell_array(dut: SimHandleBase) -> None:
     dut.rst.value = constants.HIGH
     dut.row_select.value = constants.LOW
     dut.col_select.value = constants.LOW
     dut.write_enable.value = constants.LOW
     dut.data_in.value = constants.LOW
     dut.clk.value = constants.LOW
+
     await Timer(1, unit=constants.NANOSECONDS)
     dut.rst.value = constants.LOW
     await Timer(1, unit=constants.NANOSECONDS)
     cocotb.log.info("Array reset complete")
 
-async def write_byte(dut, row, col, data):
+async def write_byte(dut: SimHandleBase, row: int, col: int, data: int) -> None:
     dut.row_select.value = row
     dut.col_select.value = col
     dut.write_enable.value = 0xFF
     dut.data_in.value = data
+
     await generate_clock_cycle(dut)
     dut.write_enable.value = constants.LOW
 
-async def read_byte(dut, row, col):
+async def read_byte(dut: SimHandleBase, row: int, col: int) -> int:
     dut.row_select.value = row
     dut.col_select.value = col
     dut.write_enable.value = constants.LOW
+
     await Timer(1, unit=constants.NANOSECONDS)
     return int(dut.data_out.value)
 
@@ -42,13 +46,17 @@ async def read_byte(dut, row, col):
 
 #Verify cell array can be clocked
 @cocotb.test()
-async def smoke_test(dut):
+async def smoke_test(dut: SimHandleBase) -> None:
     for _ in range (constants.CLOCK_PERIOD_NS):
       await generate_clock_cycle(dut)
 
 @cocotb.test()
-async def test_read_write(dut):
+async def test_read_write(dut: SimHandleBase) -> None:
     await reset_cell_array(dut)
-    test_data, test_row, test_col = constants.ALL_ONES, 0, 0
+    test_data: int = constants.ALL_ONES
+    test_row: int = 0
+    test_col: int = 0
+
     await write_byte(dut, test_row, test_col, test_data)
-    assert await read_byte(dut, test_row, test_col) == test_data, f"Expected {test_data}, got {await read_byte(dut, test_row, test_col)}"
+    read_data: int = await read_byte(dut, test_row, test_col)
+    assert read_data == test_data, f"Expected {test_data}, got {read_data}"
--- tests/constants.py
+++ tests/constants.py
@@ -4,22 +4,22 @@ This file should contain all constants used in testbenching.
 """
 
 #Hardware parameters (MUST MATCH WITH RTL)
-ROWS = 64
-COLS = 64
-DATA_WIDTH = 8
-ROW_ADDR_WIDTH = math.ceil(math.log2(ROWS))
-COL_ADDR_WIDTH = math.ceil(math.log2(COLS // DATA_WIDTH))
+ROWS: int  = 64
+COLS: int = 64
+DATA_WIDTH: int = 8
+ROW_ADDR_WIDTH: int= math.ceil(math.log2(ROWS))
+COL_ADDR_WIDTH: int = math.ceil(math.log2(COLS // DATA_WIDTH))
 
 #Test timing constants
-NANOSECONDS = "ns"
-CLOCK_PERIOD_NS = 1000
+NANOSECONDS: str = "ns"
+CLOCK_PERIOD_NS: int = 1000
 
 #Logic Levels
-LOW = 0
-HIGH = 1
+LOW: int = 0
+HIGH: int = 1
 
 # 8-bit data patterns
-ALL_ZEROES = 0x00
-ALL_ONES = 0xFF
-CHECKERBOARD_A = 0x55  # 01010101
-CHECKERBOARD_B = 0xAA  # 10101010
+ALL_ZEROES: int = 0x00
+ALL_ONES: int = 0xFF
+CHECKERBOARD_A: int = 0x55  # 01010101
+CHECKERBOARD_B: int = 0xAA  # 10101010
