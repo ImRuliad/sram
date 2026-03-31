87f6394d170b40e4578e5ce74f26d37e374f2a4a
ImRuliad <nicholaseovva@csus.edu>
2026-03-30 19:54:09 -0700

    refactor: move common tests to utils

--- tests/utils.py
+++ tests/utils.py
@@ -5,15 +5,13 @@ from cocotb.triggers import Timer
 import tests.constants as constants
 
 
-@cocotb.test()
+
 async def generate_clock_cycle(dut: SimHandleBase) -> None:
     dut.clk.value = constants.LOW
     await Timer(1, unit=constants.NANOSECONDS)
     dut.clk.value = constants.HIGH
     await Timer(1, unit=constants.NANOSECONDS)
 
-
-@cocotb.test()
 async def smoke_test(dut: SimHandleBase) -> None:
     for _ in range(constants.CLOCK_PERIOD_NS):
         await generate_clock_cycle(dut)
